ddbf990b400cca924f3ab1bea17886c5a6d393de
Jayesh Khairnar <jayeshkhairnar6@gmail.com>
2024-08-02 16:02:19 +0530

    landing

---
+++ src/components/Header.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { Box, Button, styled, Typography } from "@mui/material";
+import { Link } from 'react-router-dom'
+//img
+import headerImg from '../assets/pexels-binyamin-mellish-186078.png'
+
+const Header = () => {
+
+    const CustomBox = styled(Box) (({ theme }) => ({
+        minHeight: '80vh',
+        display: 'flex',
+        justifyContent: 'center',
+        // tamanhos
+        gap: theme.spacing(2),
+        paddingTop: theme.spacing(10),
+        // cor de fundo
+        backgroundColor: 'orange',
+        [theme.breakpoints.down('md')]: {
+            flexDirection: 'column',
+            alignItems: 'center',
+            textAlign: 'center',
+        }
+    }));
+
+    const BoxText = styled(Box) (({ theme }) => ({
+        flex: '1',
+        paddingLeft: theme.spacing(8),
+        [theme.breakpoints.down('md')]: {
+            flex: '2',
+            textAlign: 'center',
+            paddingLeft: theme.spacing(2),
+            paddingRight: theme.spacing(2),
+        },
+    }));
+
+
+  return  (
+        <CustomBox component='header'>
+            {/*  Box text  */}
+            <BoxText 
+            component='section'
+            >
+                <Typography
+                variant='h2'
+                component= 'h1'
+                sx={{
+                    fontWeight: 700,
+                    color: '#fff',
+                }}
+                >
+                    Let's configure your car with us
+                </Typography>
+
+                <Typography
+                variant='p'
+                component='p'
+                sx={{
+                    py: 3,
+                    lineHeight: 1.6,
+                    color: '#fff',
+                }}
+                >
+                    We have 500+ happy customers who
+                    trust in our service.
+                </Typography>
+
+                <Box>
+                    
+                </Box>
+            </BoxText>
+
+            <Box sx={theme => ({
+                [theme.breakpoints.down('md')]:{
+                    flex: '1',
+                    paddingTop: '30px',
+                    alignSelf: 'center',
+                },
+                [theme.breakpoints.up('md')]:{
+                    flex: '2',
+                    alignSelf: 'flex-end',
+                },
+            })}
+            >
+                <img
+                src={headerImg}
+                alt="headerImg"
+                style={{ 
+                    width: "100%", 
+                    marginBottom: -15,
+                }}
+                />
+            </Box>
+
+        </CustomBox>
+    )
+}
+
+export default Header
